d1865ccc556478c9ebc7f0cd6a041bf77a91b274
Popovych Mariia <mariia.popovych@powercoders.org>
2024-09-27 10:59:22 +0200

    Play with for loop

---
+++ Mariia/ex4110.js
@@ -0,0 +1,13 @@
+let launchCodeString = "LaunchCode";
+let launchCodeArray = [1, 5, 'LC101', 'blue', 42];
+
+
+for (let i = 0; i < launchCodeArray.length; i++) {
+  console.log(launchCodeArray[i]);
+}
+
+
+for (let i = launchCodeString.length - 1; i >= 0; i--) {
+  console.log(launchCodeString[i]);
+}
+
