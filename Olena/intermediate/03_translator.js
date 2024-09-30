769631eb5583c83bafe0323156d40736ede6a3bf
olli20 <olena.voina@powercoders.org>
2024-09-30 08:41:39 +0200

    add ex 4.2.-; 4.1.11 shuttle with class; upd prompt function

---
+++ Olena/intermediate/03_translator.js
@@ -0,0 +1,78 @@
+const readline = require('readline');
+
+const dictionary = require('./dictionary');
+
+const availableLanguages = dictionary.map(item => item.id);
+
+const promptUser = (question) => {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    
+    return new Promise((resolve) => {
+        rl.question(question, (answer) => {
+            rl.close();
+            resolve(answer.trim());
+        });
+    });
+};
+
+const askForLanguage = async () => {
+    let userInput;
+
+    do {
+        try {
+            userInput = await promptUser(`Please choose the language (${availableLanguages.join(', ')}): `);
+            if (!availableLanguages.includes(userInput)) {
+                console.log(`The program does not support ${userInput}`);
+            }
+        } catch (error) {
+            console.error('Error while choosing a language: ', error.message); 
+            return null;
+        }
+    } while (!availableLanguages.includes(userInput));
+
+    return userInput;
+}
+
+const getTranslationData = (lang = 'en') => {
+    const languageData = dictionary.find(item => item.id === lang);
+    
+    if (!languageData) {
+        console.log('Language not found, default in English.');
+        return dictionary.find(item => item.id === 'en');
+    }
+
+    return languageData;
+}
+
+const main = async () => {
+    try {
+        const selectedLanguage = await askForLanguage();
+
+        if (!selectedLanguage) {
+            console.log('An unexpected error occurred.');
+            return;
+        }
+
+        const translationData = getTranslationData(selectedLanguage);
+
+        const {name, text} = translationData;
+
+        console.log(`${name}: ${text}`);
+
+    } catch (error) {
+        console.error('An unexpected error occurred:', error.message);
+    } finally {
+        rl.close();
+    }
+};
+
+main();
+
+// Write a function named helloWorld that:
+// • Takes 1 argument, a language code (e.g. "es", "de", "en")
+// • Returns "Hello, World" for the given language, for at least 3 languages. It should default to returning
+// English.
+// Call the function for each of the supported languages and log the result to make sure it works.
---
+++ Olena/intermediate/07_password.js
@@ -0,0 +1,62 @@
+const readline = require('readline');
+
+const passwordRegex = /^(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*\d)[^\s]{8,}$/; // /...../  - Regular expression
+const startMessage = 'The password must: \n* be at least be 8 characters long \n* include 1 special symbol \n* include 1 number \n* not include a space or either';
+let password;
+
+const promptUser = (question) => {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    
+    return new Promise((resolve) => {
+        rl.question(question, (answer) => {
+            rl.close();
+            resolve(answer);
+        });
+    });
+};
+
+const askPassword = async () => {
+    let input;
+    let counter = 0;
+
+    do {
+        counter++;
+        input = await promptUser('Please enter a valid password: ');
+
+        // validation
+        if (!passwordRegex.test(input)) {
+            console.log(startMessage);
+        } else {
+            return input;
+        }
+
+    } while (counter <= 10);
+
+    return null;
+}
+
+const main = async () => {
+    console.log(startMessage);
+
+    password = await askPassword();
+
+    if (password === null) {
+        console.log('You have reached the maximum number of attempts.');
+        console.log('Please take a break and try again later.');
+        return;
+    } 
+    console.log('Congratulations! You password is valid and secure!');
+}
+
+main();
+
+// a) Write a program that asks the user for a password and checks if it is valid. The password must
+// • at least be 8 characters long
+// • include 1 special symbol
+// • include 1 number
+// • not include a space or either
+// b) Write a program that generates a password according to the requirements above. Use your validator
+// written in a).
